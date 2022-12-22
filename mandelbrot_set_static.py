b847b675d7babf897b68cce4709762986d617e4e
ZackBudai <64515138+ZackBudai@users.noreply.github.com>
2022-12-22 20:39:37 +1000

    More concise code

--- mandelbrot_set_static.py
+++ mandelbrot_set_static.py
@@ -25,26 +25,16 @@ def create_fractal(min_x, max_x, min_y, max_y, image, iters):
             color = mandelbrot(c, iters)
             image[y, x] = color
 
-def display_fractal(min_x, max_x, min_y, max_y, iters):
-    image = np.empty((1024, 1536), dtype=np.uint8)
+def display_fractal(min_x, max_x, min_y, max_y, iters, width, height):
+    image = np.empty((height, width), dtype=np.uint8)
     create_fractal(min_x, max_x, min_y, max_y, image, iters)
     plt.imshow(image, cmap='jet', extent=(min_x, max_x, min_y, max_y))
     plt.show()
 
-# Set the initial zoom level and number of iterations
+# Set the initial zoom level, resolution, and number of iterations
 min_x, max_x, min_y, max_y = -2, 1, -1, 1
+width, height = 1024, 1536
 iters = 20
 
 # Display the initial fractal
-display_fractal(min_x, max_x, min_y, max_y, iters)
-
-# Allow the user to zoom in by clicking and dragging a rectangle on the plot
-while True:
-    try:
-        rect = plt.ginput(1, timeout=0)[0]
-        min_x, max_x = sorted([rect[0], rect[2]])
-        min_y, max_y = sorted([rect[1], rect[3]])
-        iters *= 2
-        display_fractal(min_x, max_x, min_y, max_y, iters)
-    except:
-        break
+display_fractal(min_x, max_x, min_y, max_y, iters, width, height)
